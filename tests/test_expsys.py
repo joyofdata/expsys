4212283506f3f18717d66d16f2c826130e5c0c4f
Raffael <mail@raffael-vogler.de>
2021-05-22 18:26:59 +0200

    added more test cases

--- tests/test_expsys.py
+++ tests/test_expsys.py
@@ -8,21 +8,34 @@ def test_load_from_file():
     res = load_from_file(Path(__file__).parent / "example.json")
     assert isinstance(res, QA)
 
+
 @pytest.fixture
 def qa():
     return load_from_file(Path(__file__).parent / "example.json")
 
-def test_interact(monkeypatch, capsys, qa):
+
+@pytest.mark.parametrize(
+    "input_seq, exp_res_list",
+    # fmt: off
+    [
+        ([True,  True],  ["q 1", "q 1.1", "y 1.1"]),
+        ([True,  False], ["q 1", "q 1.1", "n 1.1"]),
+        ([False, False], ["q 1", "q 1.2", "n 1.2"]),
+    ],
+    # fmt: on
+)
+def test_interact(monkeypatch, capsys, qa, input_seq, exp_res_list):
+    res_list = []
+    input_seq.reverse()  # reverse because we pop off it
     with monkeypatch.context() as mc:
-        res = []
 
         def _confirm(txt, default):
-            res.append(txt)
-            return True
+            res_list.append(txt)
+            return input_seq.pop()
 
         mc.setattr(click, "confirm", _confirm)
         interact(qa)
         out, _ = capsys.readouterr()
-        res.append(out.strip())
+        res_list.append(out.strip())
 
-        assert res == ["q 1", "q 1.1", "y 1.1"]
+        assert res_list == exp_res_list
