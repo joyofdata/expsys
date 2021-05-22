a8e99d2bd1d70a1cdf5ccc44579978a751d39884
Raffael <mail@raffael-vogler.de>
2021-05-22 19:10:53 +0200

    0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="expsys",
-    version="0.1",
+    version="0.2",
     description="Expert System Shell",
     packages=["expsys"],
     install_requires=["click>=8.0.0", "pydantic>=1.8.0",],
